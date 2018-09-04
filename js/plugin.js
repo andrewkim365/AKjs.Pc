2d3f5f4bb11c5f6454acfb2921fe5d752e4923fa
andrewkim365@qq.com <andrewkim365@qq.com>
2018-09-04 18:06:50 +0800

    Add files via upload

--- js/plugin.js
+++ js/plugin.js
@@ -1,75 +1,88 @@
-﻿/******JS插件按需引入（注意：插件名称和插件文件名需要保持一致，而且插件文件必须要放到plugin目录里面，否则会出错！）******/
+﻿/*-----------------------------------------------AKjs_Config (全局设置）使用方法-------------------------------------------*/
+$(function () {
+    AKjs_Config({ //环境配置管理
+        MaskStyle: ["style3", "opacity07"], //1.所有弹窗背景图案选择（样式style1~8）、2.遮挡层背景的透明度（opacity01~09）
+        Responsive: true, //是否开启文字大小按屏幕尺寸自适应变化，考虑到兼容平板电脑建议开启 (开启 true, 停用 false）
+        ButtonLink: true, //通过元素中加data-href属性的方式跳转界面, 建议开启路由功能后使用。(使用button超链接 true,不使用button超链接 false）
+        animation: true, //是否开启元素里加动画参数的功能？（例：data-animation="{name: 'zoomIn', duration:1, delay: 0}"） 动画库：akjs.animate.css
+        pluginPath: "plugin/" //功能插件文件所在的目录设置
+    });
+});
+
+/******JS插件按需引入（注意：插件名称和插件文件名需要保持一致，而且插件文件必须要放到plugin目录里面，否则会出错！）******/
 /*
 参数说明：AKjs_Plugin("插件名称或插件的js文件名","该插件是否存在相关css文件？存在写css，不存在设为空。");
 * 做项目时不必要的插件可以注释处理。
 * 注：调用插件的地方在router目录里的相关html文件中的最底部。
 */
-AKjs_Plugin("AKjs_AllChecked"); //全选功能
-AKjs_Plugin("AKjs_ChangeIcon"); //图标切换功能
-AKjs_Plugin("AKjs_ChangeInput"); //输入框值互换功能
-AKjs_Plugin("AKjs_Checkbox","css"); //复选框美化
-AKjs_Plugin("AKjs_ChooseList"); //按钮模式复选和单选功能
-AKjs_Plugin("AKjs_Circliful"); //圆形统计插件
-AKjs_Plugin("AKjs_Codeval","css"); //图片验证功能
-AKjs_Plugin("AKjs_CountDown"); //倒计时功能
-AKjs_Plugin("AKjs_CountTo"); //数字动画效果
-AKjs_Plugin("AKjs_DateTime","css"); //日期和时间插件
-AKjs_Plugin("AKjs_Dialog","css"); //alert，confirm，prompt等弹出层功能
-AKjs_Plugin("AKjs_DropLoad","css"); //上拉刷新和下拉加载更多功能
-AKjs_Plugin("AKjs_DropUpDown"); //列表展开和收起功能
-AKjs_Plugin("AKjs_Favorite"); //点赞或收藏功能
-AKjs_Plugin("AKjs_Form"); //表单提交相关效果
-AKjs_Plugin("AKjs_GetVerifyCode"); //手机短信验证码认证功能
-AKjs_Plugin("AKjs_IntlTelInput","css"); //国际区号选择功能
-AKjs_Plugin("AKjs_Keyboard","css"); //安全键盘
-AKjs_Plugin("AKjs_Lazyload"); //图片加载时有动画效果
-AKjs_Plugin("AKjs_lightSlider","css"); //图片列表左右滑动功能
-AKjs_Plugin("AKjs_Loader","css"); //Loading效果功能
-AKjs_Plugin("AKjs_Marquee","css"); //上下左右滚动插件
-AKjs_Plugin("AKjs_MobileChat","css"); //移动端聊天功能插件
-AKjs_Plugin("AKjs_MultiDate","css"); //多功能日期和时间插件
-AKjs_Plugin("AKjs_NavScroll"); //导航滑动功能（今日头条）
-AKjs_Plugin("AKjs_NowTime"); //获取当前时间的功能
-AKjs_Plugin("AKjs_Paginator","css"); //分页插件
-AKjs_Plugin("AKjs_Popupwin"); //弹窗功能
-AKjs_Plugin("AKjs_PortraitImage","css"); //头像上传功能
-AKjs_Plugin("AKjs_PreviewImage","css"); //同时上传多个图片的功能
-AKjs_Plugin("AKjs_Progress","css"); //进度条插件
-AKjs_Plugin("AKjs_QRcode"); //二维码生成插件
-AKjs_Plugin("AKjs_Radio","css"); //单选框美化
-AKjs_Plugin("AKjs_Range","css"); //滑块功能插件
-AKjs_Plugin("AKjs_Ratyli"); //星级评价功能
-AKjs_Plugin("AKjs_ScrollFixed"); //当屏幕滑动时把指定元素固定到页面顶部的功能
-AKjs_Plugin("AKjs_Select","css"); //下拉菜单功能
-AKjs_Plugin("AKjs_SelectOption","css"); //pc端专用select下拉框插件
-AKjs_Plugin("AKjs_Slider","css"); //欢迎页和联播图功能
-AKjs_Plugin("AKjs_SnInput"); //输入支付密码功能
-AKjs_Plugin("AKjs_Spinner"); //数量控制功能
-AKjs_Plugin("AKjs_StepOrder"); //步骤插件
-AKjs_Plugin("AKjs_Substring"); //输入框里输入的数字强行转换为人民币格式的插件
-AKjs_Plugin("AKjs_Switch","css"); //开关按钮美化
-AKjs_Plugin("AKjs_Tabs"); //TABS切换内容功能
-AKjs_Plugin("AKjs_Template"); //模板引擎插件
-AKjs_Plugin("AKjs_Textarea"); //多行输入框实时查询字符数的功能
-AKjs_Plugin("AKjs_TimeAxis","css"); //时间轴展示功能
-AKjs_Plugin("AKjs_TouchDelete","css"); //列表滑动删除功能
-AKjs_Plugin("AKjs_Typeahead","css"); //模糊搜索功能
-AKjs_Plugin("AKjs_TypeIt","css"); //文字打字效果（！！！！！！！！！！样式有改动）
-AKjs_Plugin("AKjs_Viewer","css"); //图片放大预览功能
-AKjs_Plugin("AKjs_Vticker"); //列表垂直滚动插件
-AKjs_Plugin("AKjs_WebToast","css"); //提示框效果
+$(function () {
+    AKjs_Plugin("AKjs_AllChecked"); //全选功能
+    AKjs_Plugin("AKjs_ChangeIcon"); //图标切换功能
+    AKjs_Plugin("AKjs_ChangeInput"); //输入框值互换功能
+    AKjs_Plugin("AKjs_Checkbox","css"); //复选框美化
+    AKjs_Plugin("AKjs_ChooseList"); //按钮模式复选和单选功能
+    AKjs_Plugin("AKjs_Circliful"); //圆形统计插件
+    AKjs_Plugin("AKjs_Codeval","css"); //图片验证功能
+    AKjs_Plugin("AKjs_CountDown"); //倒计时功能
+    AKjs_Plugin("AKjs_CountTo"); //数字动画效果
+    AKjs_Plugin("AKjs_DateTime","css"); //日期和时间插件
+    AKjs_Plugin("AKjs_Dialog","css"); //alert，confirm，prompt等弹出层功能
+    AKjs_Plugin("AKjs_DropLoad","css"); //上拉刷新和下拉加载更多功能
+    AKjs_Plugin("AKjs_DropUpDown"); //列表展开和收起功能
+    AKjs_Plugin("AKjs_Favorite"); //点赞或收藏功能
+    AKjs_Plugin("AKjs_Form"); //表单提交相关效果
+    AKjs_Plugin("AKjs_GetVerifyCode"); //手机短信验证码认证功能
+    AKjs_Plugin("AKjs_IntlTelInput","css"); //国际区号选择功能
+    AKjs_Plugin("AKjs_Keyboard","css"); //安全键盘
+    AKjs_Plugin("AKjs_Lazyload"); //图片加载时有动画效果
+    AKjs_Plugin("AKjs_lightSlider","css"); //图片列表左右滑动功能
+    AKjs_Plugin("AKjs_Loader","css"); //Loading效果功能
+    AKjs_Plugin("AKjs_Marquee","css"); //上下左右滚动插件
+    AKjs_Plugin("AKjs_MobileChat","css"); //移动端聊天功能插件
+    AKjs_Plugin("AKjs_MultiDate","css"); //多功能日期和时间插件
+    AKjs_Plugin("AKjs_NavScroll"); //导航滑动功能（今日头条）
+    AKjs_Plugin("AKjs_NowTime"); //获取当前时间的功能
+    AKjs_Plugin("AKjs_Paginator","css"); //分页插件
+    AKjs_Plugin("AKjs_Popupwin"); //弹窗功能
+    AKjs_Plugin("AKjs_PortraitImage","css"); //头像上传功能
+    AKjs_Plugin("AKjs_PreviewImage","css"); //同时上传多个图片的功能
+    AKjs_Plugin("AKjs_Progress","css"); //进度条插件
+    AKjs_Plugin("AKjs_QRcode"); //二维码生成插件
+    AKjs_Plugin("AKjs_Radio","css"); //单选框美化
+    AKjs_Plugin("AKjs_Range","css"); //滑块功能插件
+    AKjs_Plugin("AKjs_Ratyli"); //星级评价功能
+    AKjs_Plugin("AKjs_ScrollFixed"); //当屏幕滑动时把指定元素固定到页面顶部的功能
+    AKjs_Plugin("AKjs_Select","css"); //下拉菜单功能
+    AKjs_Plugin("AKjs_SelectOption","css"); //pc端专用select下拉框插件
+    AKjs_Plugin("AKjs_Slider","css"); //欢迎页和联播图功能
+    AKjs_Plugin("AKjs_SnInput"); //输入支付密码功能
+    AKjs_Plugin("AKjs_Spinner"); //数量控制功能
+    AKjs_Plugin("AKjs_StepOrder"); //步骤插件
+    AKjs_Plugin("AKjs_Substring"); //输入框里输入的数字强行转换为人民币格式的插件
+    AKjs_Plugin("AKjs_Switch","css"); //开关按钮美化
+    AKjs_Plugin("AKjs_Tabs"); //TABS切换内容功能
+    AKjs_Plugin("AKjs_Template"); //模板引擎插件
+    AKjs_Plugin("AKjs_Textarea"); //多行输入框实时查询字符数的功能
+    AKjs_Plugin("AKjs_TimeAxis","css"); //时间轴展示功能
+    AKjs_Plugin("AKjs_TouchDelete","css"); //列表滑动删除功能
+    AKjs_Plugin("AKjs_Typeahead","css"); //模糊搜索功能
+    AKjs_Plugin("AKjs_TypeIt","css"); //文字打字效果（！！！！！！！！！！样式有改动）
+    AKjs_Plugin("AKjs_Viewer","css"); //图片放大预览功能
+    AKjs_Plugin("AKjs_Vticker"); //列表垂直滚动插件
+    AKjs_Plugin("AKjs_WebToast","css"); //提示框效果
 
-AKjs_Plugin("AKjs_Menu"); //导航状态控制功能
-AKjs_Plugin("AKjs_NavMenu"); //导航菜单效果插件
-AKjs_Plugin("AKjs_Scrollbar"); //滚动条美化
-AKjs_Plugin("AKjs_MediaElement","css"); //视频播放器
-AKjs_Plugin("AKjs_FullScreen"); //全屏功能
-AKjs_Plugin("AKjs_ElementControl"); //元素控制插件
-AKjs_Plugin("AKjs_ToolTip","css"); //提示工具插件
-AKjs_Plugin("AKjs_Mkinfinite"); //动画效果联播图插件
-AKjs_Plugin("AKjs_HoverBorder"); //布局边框动画插件
-AKjs_Plugin("AKjs_GoTop"); //返回页面顶部插件
-AKjs_Plugin("AKjs_ImgSubject"); //图片列表滑动效果
-AKjs_Plugin("AKjs_SelectOption","css"); //select下拉框插件
+    AKjs_Plugin("AKjs_Menu"); //导航状态控制功能
+    AKjs_Plugin("AKjs_NavMenu"); //导航菜单效果插件
+    AKjs_Plugin("AKjs_Scrollbar"); //滚动条美化
+    AKjs_Plugin("AKjs_MediaElement","css"); //视频播放器
+    AKjs_Plugin("AKjs_FullScreen"); //全屏功能
+    AKjs_Plugin("AKjs_ElementControl"); //元素控制插件
+    AKjs_Plugin("AKjs_ToolTip","css"); //提示工具插件
+    AKjs_Plugin("AKjs_Mkinfinite"); //动画效果联播图插件
+    AKjs_Plugin("AKjs_HoverBorder"); //布局边框动画插件
+    AKjs_Plugin("AKjs_GoTop"); //返回页面顶部插件
+    AKjs_Plugin("AKjs_ImgSubject"); //图片列表滑动效果
+    AKjs_Plugin("AKjs_SelectOption","css"); //select下拉框插件
 
 //AKjs_Plugin("AKjs_Validate"); //表单验证功能 （已停用：被AKjs_Form合并）
+});
--- js/router.js
+++ js/router.js
@@ -20,18 +20,6 @@
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
 $(document).ready(function(){
-    /*-----------------------------------------------AKjs_Config (全局设置）使用方法-------------------------------------------*/
-    AKjs_Config({ //环境配置管理
-        MaskStyle: ["style3", "opacity07"], //1.所有弹窗背景图案选择（样式style1~8）、2.遮挡层背景的透明度（opacity01~09）
-        Responsive: true, //是否开启文字大小按屏幕尺寸自适应变化，考虑到兼容平板电脑建议开启 (开启 true, 停用 false）
-        ButtonLink: true, //通过元素中加data-href属性的方式跳转界面, 建议开启路由功能后使用。(使用button超链接 true,不使用button超链接 false）
-        animation: true, //是否开启元素里加动画参数的功能？（例：data-animation="{name: 'zoomIn', duration:1, delay: 0}"） 动画库：akjs.animate.css
-        pluginPath: "./plugin/" //功能插件文件所在的目录设置
-    });
-
-    AKjs_Include("css/theme.default.css"); //颜色相关样式文件引入（AKjs_Include是js文件中引入另一个js或css文件的功能）
-    AKjs_Include("js/data.js"); //Json数据文件引入（AKjs_Include是js文件中引入另一个js或css文件的功能）
-
     /*-----------------------------------------------AKjs_Router (路由全局设置）使用方法-------------------------------------------*/
     AKjs_Router({ //路由配置管理
         Router: true, //是否开启路由（开启路由URL中带#的路径访问页面不刷新页面形式跳转 (开启 true, 停用 false）
@@ -41,34 +29,40 @@ $(document).ready(function(){
         ErrorMsg: "很抱歉，您要访问的界面加载失败！请稍后再试。", //界面加载失败时提示的信息 （找不到相关页面或者网络环境不稳定时提示的信息）
         RouterPath:["router","layout/main.html"], //路由目录和界面布局文件设置（第1个参数是路由目录文件夹名，第2个参数是指定整个界面布局的文件）
         changePage: function (hash,change) { //路由初始化调用和页面变化时的回调（公共插件引入的区域）
-            $(function () {
-                if (!hash) { //首次访问的界面您要跳转到哪个界面？
-                    if (IsMobile) { //判断是否通过移动设备访问的
-                        AKjs_Location("/page1");
-                    } else {
-                        AKjs_Location("/page1"); //location.replace 跳转模式
-                    }
-                }
-                AKjs_Include("js/plugin.js"); //功能插件按需引入（AKjs_Include是js文件中引入另一个js或css文件的功能）
-                if (change) { //change是用于判断hash模式是否跳页
-                    /*-----------------------------------------------AKjs_Loader 使用方法-------------------------------------------*/
-                    AKjs_Loader({
-                        ele: $("main").children("#ak-main"), //是否使用局部遮挡层，使用请设置指定的局部元素 （不设置任何参数代表使用全部遮挡层）
-                        autoMode: true, //是否开启指定的时间后自动消失功能 (开启 true, 关闭 false）
-                        timeToHide: 1000, //毫秒时间设置 (automode必须开启才能有效)
-                        iconColor:"#ffffff", //图标颜色设置
-                        maskBG: false, //是否开启遮挡背景 (开启 true, 关闭 false）
-                        Loader:"load_2" //loading效果选择（load_1~7）
-                    });
-                    $(document).on('click', '.ak-loading', function () {
-                        AKjs_Loader("destroy"); //关闭loading窗
-                    });
-                }
-
-                /*-----------------------------------------------AKjs_Scrollbar 使用方法-------------------------------------------*/
-                if (IsIE6 || IsIE7 || IsIE8) {
-                    $("aside").addClass("ovs_im scrollbar");
+            if (!hash) { //首次访问的界面您要跳转到哪个界面？
+                if (IsMobile) { //判断是否通过移动设备访问的
+                    AKjs_Location("/page1"); //location.replace 跳转模式
                 } else {
+                    AKjs_Location("/page1"); //location.replace 跳转模式
+                }
+            }
+            if (!change) { //change是用于判断hash模式是否跳页
+                AKjs_Include("css/theme.default.css"); //颜色相关样式文件引入（AKjs_Include是js文件中引入另一个js或css文件的功能）
+                AKjs_Include("js/data.js"); //Json数据文件引入（AKjs_Include是js文件中引入另一个js或css文件的功能）
+            }
+
+            AKjs_Include("js/plugin.js"); //功能插件按需引入（为了正常运行功能插件通过AKjs_Include方式引入）
+
+            /*-----------------------------------------------AKjs_Loader 使用方法-------------------------------------------*/
+            $(function() {
+                AKjs_Loader({
+                    ele: $("main").children("#ak-main"), //是否使用局部遮挡层，使用请设置指定的局部元素 （不设置任何参数代表使用全部遮挡层）
+                    autoMode: true, //是否开启指定的时间后自动消失功能 (开启 true, 关闭 false）
+                    timeToHide: 1000, //毫秒时间设置 (automode必须开启才能有效)
+                    iconColor:"#ffffff", //图标颜色设置
+                    maskBG: false, //是否开启遮挡背景 (开启 true, 关闭 false）
+                    Loader:"load_2" //loading效果选择（load_1~7）
+                });
+            });
+            $(document).on('click', '.ak-loading', function () {
+                AKjs_Loader("destroy"); //关闭loading窗
+            });
+
+            /*-----------------------------------------------AKjs_Scrollbar 使用方法-------------------------------------------*/
+            if (IsIE6 || IsIE7 || IsIE8) {
+                $("aside").addClass("ovs_im scrollbar");
+            } else {
+                $(function() {
                     $("aside").AKjs_Scrollbar({
                         children: ".plug_scroll",
                         speed: 25,
@@ -87,8 +81,10 @@ $(document).ready(function(){
                             //console.log(bartop) /*滚动条的位置*/
                         }
                     });
-                }
-                /*-----------------------------------------------AKjs_Menu (菜单控制插件）使用方法-------------------------------------------*/
+                });
+            }
+            /*-----------------------------------------------AKjs_Menu (菜单控制插件）使用方法-------------------------------------------*/
+            $(function() {
                 $(".plug_nav li").AKjs_Menu({ //底部菜单的图标以及文字样式变化设置
                     icon_text: ["dt i","dt span"], //设置需要控制的菜单图标和文字元素
                     btn_color: "hover_gray_222 c_white", //未选中的文字和图标的颜色
@@ -97,8 +93,10 @@ $(document).ready(function(){
                         console.log(ele, index);
                     }
                 });
+            });
 
-                /*-----------------------------------------------AKjs_Lazyload 使用方法-------------------------------------------*/
+            /*-----------------------------------------------AKjs_Lazyload 使用方法-------------------------------------------*/
+            $(function() {
                 $("*[data-animation]").AKjs_Lazyload({ //对所有带data-animation属性的元素进行懒加载，让滚动条位置到达该元素区域时动画播放；
                     scroll: $("main"), //滚动区域的容器
                     scrollTop: 100, //设置初始化滚动条位置（当滚动条滚动到当前设置的位置时所有效果将进行初始化）
@@ -109,6 +107,8 @@ $(document).ready(function(){
                         //console.log(scroll);
                     }
                 });
+            });
+            $(function() {
                 $("img").AKjs_Lazyload({ //对所有的图片懒加载
                     scroll: $("main"), //滚动区域的容器
                     scrollTop: 100, //设置初始化滚动条位置（当滚动条滚动到当前设置的位置时所有效果将进行初始化）
@@ -122,41 +122,41 @@ $(document).ready(function(){
                         //console.log(ele);
                     }
                 });
-                ak_mainHeight(); //重新设置main元素的高度
-                /*
-                     AKjs_Location 使用方法：
-                    //url=跳转路径，{type=跳转类型（href,history,reload），time=延迟时间，router=页面切换效果（left,right）}
-
-                    AKjs_Location("url",{type:"", time: "", router:""}); //参数设置结构
-                    AKjs_Location("/start"); //location.replace 跳转模式(第二个参数默认识别time参数)
-                    AKjs_Location("/start",{type:"href"}); //location.href 跳转模式
-                    AKjs_Location("/",{type:"reload"}); //location.reload() 刷新当前页
-                    AKjs_Location("-1",{type:"history"}); //history.back(-1) 跳转返回上一页,也可以设置0，-2 等数值
-                    AKjs_Location("/start",{time:1000}); //location.replace 跳转模式 (延迟1秒后跳转)
-                    AKjs_Location("/start",{router:left}); //location.replace 跳转模式 (router参数是页面切换效果，left是左滑[返回效果]，right是右滑[进入效果])
-
-                    AKjs_getUrlParam &  AKjs_changeURLArg 使用方法：
-                    console.log("GET_ak: "+AKjs_getUrlParam('ak')); //获取URL中的参数值
-                    console.log(AKjs_changeURLArg(location.hash,"ak","change"+AKjs_getUrlParam('ak'))); //更改URL中的参数值
-
-                    AKjs_Unicode 使用方法：
-                    console.log(AKjs_Unicode("中文字符"));
-
-                     AKjs_setCookie & AKjs_getCookie & AKjs_delCookie 使用方法：
-                     AKjs_setCookie("username", user, 365); //设置cookie
-                     var user = AKjs_getCookie("username"); //获取cookie
-                     AKjs_delCookie(name) //删除cookie
-
-                     AKjs_htmlEncode & AKjs_htmlDecode 使用方法：
-                     AKjs_htmlEncode(str); //把TEXT转换HTML
-                     AKjs_htmlDecode(str); //把HTML转换TEXT
-
-                     AKjs_DateFormat & AKjs_FileFormat 使用方法：
-                     console.log(AKjs_DateFormat("2018/03/30 17:50","yyyy-MM-dd HH:mm"));
-                     console.log(AKjs_DateFormat(new Date(),"yyyy-MM-dd HH:mm"));
-                     AKjs_FileFormat(filename) //获取文件的扩展名
-                 */
             });
+            ak_mainHeight(); //重新设置main元素的高度
+            /*
+                 AKjs_Location 使用方法：
+                //url=跳转路径，{type=跳转类型（href,history,reload），time=延迟时间，router=页面切换效果（left,right）}
+
+                AKjs_Location("url",{type:"", time: "", router:""}); //参数设置结构
+                AKjs_Location("/start"); //location.replace 跳转模式(第二个参数默认识别time参数)
+                AKjs_Location("/start",{type:"href"}); //location.href 跳转模式
+                AKjs_Location("/",{type:"reload"}); //location.reload() 刷新当前页
+                AKjs_Location("-1",{type:"history"}); //history.back(-1) 跳转返回上一页,也可以设置0，-2 等数值
+                AKjs_Location("/start",{time:1000}); //location.replace 跳转模式 (延迟1秒后跳转)
+                AKjs_Location("/start",{router:left}); //location.replace 跳转模式 (router参数是页面切换效果，left是左滑[返回效果]，right是右滑[进入效果])
+
+                AKjs_getUrlParam &  AKjs_changeURLArg 使用方法：
+                console.log("GET_ak: "+AKjs_getUrlParam('ak')); //获取URL中的参数值
+                console.log(AKjs_changeURLArg(location.hash,"ak","change"+AKjs_getUrlParam('ak'))); //更改URL中的参数值
+
+                AKjs_Unicode 使用方法：
+                console.log(AKjs_Unicode("中文字符"));
+
+                 AKjs_setCookie & AKjs_getCookie & AKjs_delCookie 使用方法：
+                 AKjs_setCookie("username", user, 365); //设置cookie
+                 var user = AKjs_getCookie("username"); //获取cookie
+                 AKjs_delCookie(name) //删除cookie
+
+                 AKjs_htmlEncode & AKjs_htmlDecode 使用方法：
+                 AKjs_htmlEncode(str); //把TEXT转换HTML
+                 AKjs_htmlDecode(str); //把HTML转换TEXT
+
+                 AKjs_DateFormat & AKjs_FileFormat 使用方法：
+                 console.log(AKjs_DateFormat("2018/03/30 17:50","yyyy-MM-dd HH:mm"));
+                 console.log(AKjs_DateFormat(new Date(),"yyyy-MM-dd HH:mm"));
+                 AKjs_FileFormat(filename) //获取文件的扩展名
+             */
         },
         error:function (hash) { //请求加载页面失败后的回调 （可删除该回调入口）
             if (hash) { //获取hash的参数值，当前的判断是hash有值的情况
