be22fee8ba5d0eb01e302297cc8e1dde0776f563
andrewkim365@qq.com <andrewkim365@qq.com>
2018-09-13 11:26:44 +0800

    Add files via upload

--- plugin/AKjs_Textarea.js
+++ plugin/AKjs_Textarea.js
@@ -1,5 +1,5 @@
 ﻿/*
-Modification Date: 2018-09-10
+Modification Date: 2018-08-09
 Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
 */
 /*-----------------------------------------------AKjs_Textarea----------------------------------------*/
@@ -11,47 +11,25 @@ Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
                 onTextVal: function() {}
             },
             setting);
-        AKjs_UserAgent();
         var txt =$(this);
         if (txt.length > 0) {
             txt.each(function(){ //input元素加maxlength属性后控制自定义字数
-                var _this = $(this);
-                _this.next("span").remove();
                 $(this).after('<span class="dis_block_im ovh abs center text_al_r text_08em">' +
                     '<var class="text_08em" style="color: #f16a6a;">0</var>' +
                     '/' +
                     '<var class="text_08em mr_03em">'+opm.maxlength+'</var>' +
                     '</span>');
-                if (IsMobile) {
-                    _this.next("span").css({
-                        "width": _this.width(),
-                        "left": ($(window).width() - _this.width()) /2
-                    });
-                    $(window).resize(function(){
-                        _this.next("span").css({
-                            "width": _this.width(),
-                            "left": ($(window).width() - _this.width()) /2
-                        });
-                    });
-                } else {
-                    _this.next("span").css({
-                        "width": _this.parent().width() - 10,
-                        "left": 0
-                    });
-                    $(window).resize(function(){
-                        _this.next("span").css({
-                            "width": _this.parent().width() - 10,
-                            "left": 0
-                        });
-                    });
-                }
+                $(this).next("span").css({
+                    "width": $(this).width(),
+                    "left": ($(window).width() - $(this).width()) /2
+                });
                 $(this).attr("rows",opm.rows).attr("maxlength",opm.maxlength);
                 $(this).parent().css({
                     "padding-bottom": $(this).next("span").height() * 1.2+"px"
                 })
             });
             var len = 0;
-            txt.keyup(function() {
+            txt.on("input propertychange",function(e){
                 if( len >= opm.maxlength && e.keyCode == 8 ){
                     return;
                 }else{
