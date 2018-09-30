b4602cf8aa051892080b556f11d2d099c14a3de6
andrewkim365@qq.com <andrewkim365@qq.com>
2018-09-30 10:05:54 +0800

    Add files via upload

--- plugin/AKjs_ElementControl.js
+++ plugin/AKjs_ElementControl.js
@@ -1,42 +1,80 @@
 ﻿/*
-Modification Date: 2018-09-17
+Modification Date: 2018-09-30
 Coding by Andrew.Kim (E-mail: andrewkim365@qq.com)
 */
 /*-----------------------------------------------AKjs_ElementControl--------------------------------------------*/
 (function($){
-    var Plugin = function(ele, options) {
-        this.$ele = ele;
+    var Plugin = function(_this, setting) {
+        this.$_this = _this;
         this.defaults = {
-            dom:"",
-            mouse: 'click',
-            clickCallback: function() {},
-            hoverCallback: function() {}
+            ControlType: 'click',
+            ControlDom:"",
+            scrollTop: 0,
+            callback:function(){
+            },
+            clickback: function() {
+            },
+            hoverback: function() {
+            },
+            scrollback:function(){
+            }
         };
-        this.opts = $.extend({},
-            this.defaults, options);
+        this.option = $.extend({},
+            this.defaults, setting);
     };
     Plugin.prototype = {
         inital: function() {
             var self = this;
-            if (self.opts.mouse === 'click') {
-                this.$ele.click(function() {
-                    self.opts.clickCallback(self.$ele,self.opts.dom);
+            if (self.option.ControlType === 'click') {
+                $(function() {
+                    click_fun();
                 });
-            } else if (self.opts.mouse === 'hover') {
-                this.$ele.mouseover(function(){
-                    hover = true;
-                    self.opts.hoverCallback(self.$ele, self.opts.dom, hover);
+                $(window).resize(function () {
+                    click_fun();
                 });
-                this.$ele.mouseout(function(){
-                    hover = false;
-                    self.opts.hoverCallback(self.$ele, self.opts.dom, hover);
+                function click_fun() {
+                    self.option.callback($(self.option.ControlDom),self.$_this);
+                    self.$_this.click(function() {
+                        self.option.clickback($(self.option.ControlDom),self.$_this);
+                    });
+                }
+            } else if (self.option.ControlType === 'hover') {
+                $(function() {
+                    hover_fun();
                 });
+                $(window).resize(function () {
+                    hover_fun();
+                });
+                function hover_fun() {
+                    self.option.callback($(self.option.ControlDom),self.$_this);
+                    self.$_this.mouseover(function(){
+                        flag = true;
+                        self.option.hoverback($(self.option.ControlDom),self.$_this,flag);
+                    });
+                    self.$_this.mouseout(function(){
+                        flag = false;
+                        self.option.hoverback($(self.option.ControlDom),self.$_this,flag);
+                    });
+                }
+            } else if (self.option.ControlType === 'scroll') {
+                $(function() {
+                    scroll_fun();
+                });
+                $(window).resize(function () {
+                    scroll_fun();
+                });
+                function scroll_fun() {
+                    self.option.callback($(self.option.ControlDom),self.$_this);
+                    self.$_this.scroll(function(){
+                        self.option.scrollback($(self.option.ControlDom), self.$_this, $(this).scrollTop(), self.option.scrollTop);
+                    });
+                }
             }
         },
         constructor: Plugin
     };
-    $.fn.AKjs_ElementControl = function(options) {
-        var plugin = new Plugin(this, options);
-        return plugin.inital();
+    $.fn.AKjs_ElementControl = function(setting) {
+        var ak_ElemControl = new Plugin(this, setting);
+        return ak_ElemControl.inital();
     };
 }(jQuery));
